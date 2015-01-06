3733d7c03bf48a81c7a42fa3d8c4db03a70e6d7a
edgardleal <edgardleal@gmail.com>
2015-01-05 21:57:30 -0300

    Modification in build.sh script to debug
    ./build.sh test debug

--- C/build.sh
+++ C/build.sh
@@ -2,21 +2,27 @@
 
 declare targetdir="target"
 declare mysql_include="$(mysql_config --include)"
+declare mysql_flag="$(mysql_config --cflags)"
+declare mysql_libs="$(mysql_config --libs)"
 
 [ ! -d "clib" ] && git submodule add https://github.com/edgardleal/clib.git
 [ ! -d "expression_parser" ] && git submodule add https://github.com/edgardleal/expression_parser.git
 [ ! -d $targetdir ] && mkdir $targetdir
 
 if [ "$1" == "test" ]; then
-  gcc -x c -I . tests.c -o ${targetdir}/edscript_tests
+  gcc -g -x c -I . tests.c -o ${targetdir}/edscript_tests ${mysql_include} ${mysql_cflags} ${mysql_libs}
   if [ $? == 0 ]; then 
-    ./${targetdir}/edscript_tests -c
+    if [ "$2" == "debug" ]; then
+      gdb ./${targetdir}/edscript_tests
+    else
+      ./${targetdir}/edscript_tests -c
+    fi
     exit $!
   else
     echo "Erro ao compilar"
     exit 1
   fi
 else
-  gcc -x c $mysql_include  main.c -o ${targetdir}/edscript
+  gcc -x c $mysql_include ${mysql_flags} ${mysql_libs} main.c -o ${targetdir}/edscript
 fi
 
--- C/tests.c
+++ C/tests.c
@@ -6,8 +6,9 @@
 #include "libtest.h"
 #include "commands.c"
 #include "hashmap.c"
-#include "expression_parser.c"
-
+#include "expression_parser/code/expression_parser.c"
+#include "mysql_connector.c"
+#include "clib/src/stringutil.c"
 
 void test_expression(char *expression, double expected)
 {
@@ -23,11 +24,34 @@ int main(int argc, char *argv[])
     ok(ascii_hash("nome_tabela") < 8500,"%s", "Teste com a palavra nome_tabela");
     ok(ascii_hash("nova_palavra")< 8500,"%s", "Teste com a palavra nova_palavra");
 
+    
+    new_test_case("StringUtils\n");
+    char **_list;
+    string_split("mysql:host=teste;db=banco\0\0", ':', &_list);
+    ok(strcmp("mysql", _list[0]) == 0, "%s", "Conteudo a primeira parte");
+
+    new_test_case("Mysql");
+    if(!file_exists("connection.test"))
+    {
+        append_to_file("connection.test","mysql:host=localhost;db=test\nroot\nroot");
+    }
+
+    struct st_database_connection _c;
+    read_mysql_connection_from_file("connection.test", &_c);
+
+    int ret = strncmp("localhost", _c.host, 9);
+    ok(ret == 0, "%s", "Host lido do arquivo com sucesso");
+    ok(!connect(), "%s", "Conexao com o mysql efetuada com sucesso");
+
+
     new_test_case("Commands");
-    struct command c;
+    struct st_command c;
     c.line = 5;
-    append_command(c);
+    printf("adicionando o comando");
+    append_command(&c);
+    printf("command adicionado");
     ok(commands[commands_count - 1].line == 5,"%s", "Verifica a inclusao do comando");
+    printf("teste executado");
 
 
     new_test_case("Math Expressions");
